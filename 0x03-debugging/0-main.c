31c5f68f1865e9862dbd93c1f5e31904245d29ac
tarneem <tarneemahmed2002@gmail.com>
2023-04-26 10:03:45 -0700

    add

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,8 +10,19 @@ int main(void)
 {
 	int n;
 
-	n = 98 % 2;
-	positive_or_negative(n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
 
 	return (0);
 }
