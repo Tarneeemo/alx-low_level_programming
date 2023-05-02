52b64b3f4ab4d5b06717fd69fe7fd8214ba0a290
tarneem <tarneemahmed2002@gmail.com>
2023-05-02 09:40:42 -0700

    add

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rest_to_98 - takes the pointer and updates the value
+ *
+ * @n: int to check
+ *
+ * Return: 0 is success
+*/
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
