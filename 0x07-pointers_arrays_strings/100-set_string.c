0fa22fcfedec195213f3cda0e674a2dbe74ebca3
tarneem <tarneemahmed2002@gmail.com>
2023-05-08 08:08:18 -0700

    add

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of pointer to char
+ *
+ * @s: pointer to pointer we need to set to
+ * @to: string to set
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ *
+ * @c: occurrence of the char
+ * @s: in the string
+ *
+ * Return: pointer to the first occurrence of the char
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
